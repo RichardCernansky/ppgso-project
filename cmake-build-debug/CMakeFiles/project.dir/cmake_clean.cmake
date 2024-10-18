cf7deebd198e24c7cd92672848a4a24a8b83a1e0
Bruno <brunokristian003@gmail.com>
2024-10-18 13:04:18 +0200

    apple gravitation

---
+++ cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/src/generator.cpp.o"
+  "CMakeFiles/project.dir/src/generator.cpp.o.d"
+  "CMakeFiles/project.dir/src/main.cpp.o"
+  "CMakeFiles/project.dir/src/main.cpp.o.d"
+  "CMakeFiles/project.dir/src/objects/apple.cpp.o"
+  "CMakeFiles/project.dir/src/objects/apple.cpp.o.d"
+  "CMakeFiles/project.dir/src/objects/gas.cpp.o"
+  "CMakeFiles/project.dir/src/objects/gas.cpp.o.d"
+  "CMakeFiles/project.dir/src/objects/ground.cpp.o"
+  "CMakeFiles/project.dir/src/objects/ground.cpp.o.d"
+  "CMakeFiles/project.dir/src/objects/horseFly.cpp.o"
+  "CMakeFiles/project.dir/src/objects/horseFly.cpp.o.d"
+  "CMakeFiles/project.dir/src/objects/pig.cpp.o"
+  "CMakeFiles/project.dir/src/objects/pig.cpp.o.d"
+  "CMakeFiles/project.dir/src/objects/tree.cpp.o"
+  "CMakeFiles/project.dir/src/objects/tree.cpp.o.d"
+  "CMakeFiles/project.dir/src/scene.cpp.o"
+  "CMakeFiles/project.dir/src/scene.cpp.o.d"
+  "CMakeFiles/project.dir/src/window.cpp.o"
+  "CMakeFiles/project.dir/src/window.cpp.o.d"
+  "project"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
