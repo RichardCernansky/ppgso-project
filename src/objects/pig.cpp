749adf8b9d0e09fc97058dbf37f080efb7fae52a
RichardCernansky <richard.cernansky3@gmail.com>
2024-11-22 20:20:14 +0100

    minor

--- src/objects/pig.cpp
+++ src/objects/pig.cpp
@@ -7,6 +7,7 @@
 #include "src/generator.h"
 #include <random>
 #include "wolf.h"
+#include "src/globals.h"
 
 // Static resources (shared among all instances of Pig)
 std::unique_ptr<ppgso::Mesh> Pig::mesh;
@@ -160,6 +161,20 @@ bool Pig::update(float dTime, Scene &scene) {
 
 // Render method
 void Pig::render(Scene &scene) {
+    // Use the shadow projection matrix
+    glm::mat4 shadowMatrix = calculateShadowMatrix(moonLight_position, glm::vec4(0.0f, 1.0f, 0.0f, 0.0f));
+
+    // Render the shadow
+    scene.colorShader->use();
+    scene.colorShader->setUniform("ModelMatrix", shadowMatrix * modelMatrix);
+    scene.colorShader->setUniform("ViewMatrix", scene.camera->viewMatrix);
+    scene.colorShader->setUniform("ProjectionMatrix", scene.camera->perspective);
+
+    // Render the pig's shadow as a black silhouette
+    glDisable(GL_DEPTH_TEST); // Prevent z-fighting
+    scene.colorShader->setUniform("Color", glm::vec3(0.0f, 0.0f, 0.0f)); // Black shadow
+    mesh->render();
+    glEnable(GL_DEPTH_TEST);
     // Use the shader and set the transformation matrices
     scene.shader->use();
     scene.shader->setUniform("ModelMatrix", modelMatrix);
--- src/objects/wolf.cpp
+++ src/objects/wolf.cpp
@@ -2,6 +2,7 @@
 
 #include "pig.h"
 #include "src/generator.h"
+#include "src/globals.h"
 
 // Static resources (shared among all instances of Wolf)
 std::unique_ptr<ppgso::Mesh> Wolf::mesh;
@@ -117,6 +118,20 @@ bool Wolf::update(float dTime, Scene &scene) {
 
 
 void Wolf::render(Scene &scene) {
+    // Use the shadow projection matrix
+    glm::mat4 shadowMatrix = calculateShadowMatrix(moonLight_position, glm::vec4(0.0f, 1.0f, 0.0f, 0.0f));
+
+    // Render the shadow
+    scene.colorShader->use();
+    scene.colorShader->setUniform("ModelMatrix", shadowMatrix * modelMatrix);
+    scene.colorShader->setUniform("ViewMatrix", scene.camera->viewMatrix);
+    scene.colorShader->setUniform("ProjectionMatrix", scene.camera->perspective);
+
+    // Render the pig's shadow as a black silhouette
+    glDisable(GL_DEPTH_TEST); // Prevent z-fighting
+    scene.colorShader->setUniform("Color", glm::vec3(0.0f, 0.0f, 0.0f)); // Black shadow
+    mesh->render();
+    glEnable(GL_DEPTH_TEST);
     // Use the shader program
     scene.shader->use();
 
